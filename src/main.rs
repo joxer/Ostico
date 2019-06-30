7e2913982280f524eba19b001574910a4ef6668e
joxer <diego.luca.candido@gmail.com>
2019-06-30 16:18:50 +0100

    added print with spinlock

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
+use core::fmt::Write;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,16 +15,8 @@ static HELLO: &[u8] = b"hello world";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize *2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-
-        }
-
-    }
+    vga_buffer::WRITER.lock().write_str("Hello darkness my old friend").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop {}
 }
