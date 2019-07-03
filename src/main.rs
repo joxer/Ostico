62622b51663421c273d0de55dd6dd14591245b0d
joxer <diego.luca.candido@gmail.com>
2019-07-03 20:52:19 +0100

    added basic test for the project

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
+
 #![no_std]
 #![no_main]
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(ostico::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use core::fmt::Write;
+use ostico::println;
 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"hello world";
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello darkness my old friend{}", "!");
-
-    loop {}
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    ostico::test_panic_handler(info)
 }
